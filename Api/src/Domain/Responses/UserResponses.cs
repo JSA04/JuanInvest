f931dddff3a1275341324076de5655df46cf8f63
JSA04 <juan110588@gmail.com>
2024-02-16 15:51:08 -0300

    feat/user

---
+++ Api/src/Domain/Responses/UserResponses.cs
@@ -0,0 +1,8 @@
+namespace Api.Domain.Responses;
+
+public static class UserResponses
+{
+    public const string Connected = "Logado com sucesso.";
+    public const string ConnectFail = "Por conta de um erro interno no servidor, não foi possivel cadastrar a ação.";
+    public const string UserNotFound = "Combinação de úsuario e senha não encontrada.";
+}
